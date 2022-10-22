c3749bd16c5591c47e21d9dacb151288dc233f95
Rayan Albraidi <67024778+Rayan2312@users.noreply.github.com>
2022-10-21 23:47:24 -0700

    Added sizepsecs for android tablets

--- assets/Themes/index.js
+++ assets/Themes/index.js
@@ -2,4 +2,5 @@ import Profiles from '../Profiles';
 import Icons from '../Icons';
 import Themes from './themes';
 import { navBarHeight, navBarTextSize, titleTextSize, footerTextSize } from './sizeSpecs';
+import { useState } from 'react';
 export { Profiles, Icons, Themes, navBarHeight, navBarTextSize, titleTextSize, footerTextSize};
--- assets/Themes/sizeSpecs.js
+++ assets/Themes/sizeSpecs.js
@@ -1,7 +1,12 @@
 import { Platform } from "react-native";
+import { DeviceType, getDeviceTypeAsync } from "expo-device";
 
-const navBarHeight = Platform.OS === "ios"?  41 : 54;
-const navBarTextSize = Platform.isPad? 24 : 14;
-const titleTextSize = Platform.isPad? 36 : 24;
-const footerTextSize = Platform.isPad? 18 : 12;
+const deviceType = getDeviceTypeAsync();
+
+let navBarHeight = Platform.OS === "ios"?  41 : 54;
+navBarHeight = deviceType.then((type)=> type, ()=> DeviceType.UNKNOWN) === DeviceType.TABLET || Platform.isPad? 2*navBarHeight : navBarHeight;
+
+const navBarTextSize = deviceType.then((type)=> type, ()=> DeviceType.UNKNOWN) === DeviceType.TABLET || Platform.isPad? 24 : 14;
+const titleTextSize = deviceType.then((type)=> type, ()=> DeviceType.UNKNOWN) === DeviceType.TABLET ||  Platform.isPad? 44 : 24;
+const footerTextSize = deviceType.then((type)=> type, ()=> DeviceType.UNKNOWN) === DeviceType.TABLET || Platform.isPad? 18 : 12;
 export {navBarHeight, navBarTextSize, titleTextSize, footerTextSize};
